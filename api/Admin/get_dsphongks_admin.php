799e4bb5256b7069112f55221b3123a22a9f6355
jtxiee <163502813+jtxiee@users.noreply.github.com>
2024-09-17 14:15:27 +0700

    Cập nhật xem ds phong admin

---
+++ api/Admin/get_dsphongks_admin.php
@@ -0,0 +1,11 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=UTF-8");
+
+include("../../myclass/clsapi.php");
+
+$p = new clsapi();
+
+$p->Xemds_Phong("select * from rooms order by id ASC");
