edebf3cfa4a4244bcb834e0943b5319056457ab6
jtxiee <163502813+jtxiee@users.noreply.github.com>
2024-09-17 14:34:08 +0700

    Cập nhật xem ds nguoi dung admin

---
+++ api/Admin/get_usernd.php
@@ -0,0 +1,12 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=UTF-8");
+
+include("../../myclass/clsapi.php");
+
+$p = new clsapi();
+
+// Truy vấn từ bảng user_cred
+$p->Xemds_Users("SELECT * FROM user_cred ORDER BY id ASC");
