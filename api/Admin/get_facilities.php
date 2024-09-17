b379654c5f0fedc9064d47901e5f872c3a1a97d9
jtxiee <163502813+jtxiee@users.noreply.github.com>
2024-09-17 14:10:41 +0700

    Cập nhật xem facilities và features

---
+++ api/Admin/get_facilities.php
@@ -0,0 +1,12 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=UTF-8");
+
+include("../../myclass/clsapi.php");
+
+$p = new clsapi();
+
+// Truy vấn từ bảng facilities thay vì rooms
+$p->Xemds_Facilities("SELECT * FROM facilities ORDER BY id ASC");
